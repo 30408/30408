2f5a5b201b3150b2286ab73663e317784b69f122
30408 <xxqkrthdmsxxpad@gmail.com>
2025-06-11 10:05:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
+import os
 import pandas as pd
 
-# CSV 파일 읽기 (파일 경로가 정확해야 함)
-df = pd.read_csv("일산화탄소_CO__배출량_20250609093209.csv")
-
-# 데이터 확인
-print(df.head())
+csv_path = os.path.join("data", "일산화탄소_CO__배출량_20250609093209.csv")
+df = pd.read_csv(csv_path)
